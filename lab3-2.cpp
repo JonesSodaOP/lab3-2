1295900d1078e443b05bc04c03c4e9954e7e2e9b
JonesSodaOP <148819585+JonesSodaOP@users.noreply.github.com>
2023-10-26 05:11:50 +0000

    Modified and finished code

--- lab3-2.cpp
+++ lab3-2.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
+float Celsius;
+float Fahrenheit; // Float used for decimal value inputs
 
+	cout << "Enter a temperature in Fahrenheit: ";
+	cin>> Fahrenheit;
+	
+	Celsius = (Fahrenheit - 32) * 5/9;
+	
+	cout << "The temperature in Fahrenheit is: " << Fahrenheit << endl;
+	cout << "The temperature in Celsius is: " << Celsius << endl;
+	
+	return 0;
 }
+// End of program
