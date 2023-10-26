d55ecd81007bb7ca7c74bf71dc7e4374213a6652
JonesSodaOP <148819585+JonesSodaOP@users.noreply.github.com>
2023-10-26 05:15:58 +0000

    Changed comment

--- .ccls-cache/@home@runner@lab3-2/lab3-2.cpp
+++ .ccls-cache/@home@runner@lab3-2/lab3-2.cpp
@@ -8,7 +8,7 @@ float Fahrenheit; // Float used for decimal value inputs
 	cout << "Enter a temperature in Fahrenheit: ";
 	cin>> Fahrenheit;
 	
-	Celsius = (Fahrenheit - 32) * 5/9;
+	Celsius = (Fahrenheit - 32) * 5/9; // Formula for conversion
 	
 	cout << "The temperature in Fahrenheit is: " << Fahrenheit << endl;
 	cout << "The temperature in Celsius is: " << Celsius << endl;
